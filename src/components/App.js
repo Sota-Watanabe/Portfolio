808402f02d7ca3f460dd57148075d08555a2d8fe
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-09-05 10:26:20 +0900

    cssBaseline追加

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Header } from "./Header";
+import CssBaseline from "@material-ui/core/CssBaseline";
 export const App = () => {
   return (
     <>
       <Header />
+      <CssBaseline />
       <h1> test1 </h1>
       <h1> test2 </h1>
       <h1> test3 </h1>
