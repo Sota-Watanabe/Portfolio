ae350f6e9f840ba6f24edddded50948177bcb736
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-09-07 17:25:01 +0900

    sawarabiフォントの追加

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./components/App";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: ["Sawarabi Gothic", "Noto Sans", "sans-serif"].join(","),
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
