bae1573f4e383d8df5976b586da800df0380b479
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-09-12 21:37:50 +0900

    Contactの形だけ追加

---
+++ src/sections/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import EmailIcon from "@material-ui/icons/Email";
+import { makeStyles, Grid, Box, Typography } from "@material-ui/core";
+import grey from "@material-ui/core/colors/grey";
+
+import { SectionLabel } from "../components/SectionLabel";
+
+const useStyles = makeStyles((theme) => ({
+  section: {
+    backgroundColor: grey[700],
+    padding: "50px 0",
+  },
+}));
+
+export const Contact = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <SectionLabel Icon={EmailIcon} text="CONTACT" />
+      <Grid
+        container
+        justify="center"
+        alignItems="center"
+        className={classes.section}
+      >
+        <Box m={4} my={8}>
+          <Typography variant="h3">現在制作中</Typography>
+        </Box>
+      </Grid>
+    </>
+  );
+};
