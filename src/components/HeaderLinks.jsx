3fcd7d9cb52119409c7f66d52e0783bec81cf0f0
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-09-12 23:16:32 +0900

    リンク機能の追加

--- src/components/HeaderLinks.jsx
+++ src/components/HeaderLinks.jsx
@@ -4,27 +4,64 @@ import FitnessCenterIcon from "@material-ui/icons/FitnessCenter";
 import AssignmentTurnedInIcon from "@material-ui/icons/AssignmentTurnedIn";
 import EmailIcon from "@material-ui/icons/Email";
 import EmojiPeopleIcon from "@material-ui/icons/EmojiPeople";
+import { Link } from "react-scroll";
 
 export const HeaderLinks = () => {
   return (
     <Grid container>
       <Grid container item xs={6} sm={3} justify="center">
-        <Button size="large" startIcon={<EmojiPeopleIcon />}>
+        <Button
+          component={Link}
+          to="about"
+          smooth={true}
+          offset={-100}
+          duration={600}
+          activeClass="active"
+          size="large"
+          startIcon={<EmojiPeopleIcon />}
+        >
           about
         </Button>
       </Grid>
       <Grid container item xs={6} sm={3} justify="center">
-        <Button size="large" startIcon={<FitnessCenterIcon />}>
+        <Button
+          component={Link}
+          to="skills"
+          smooth={true}
+          offset={-100}
+          duration={600}
+          activeClass="active"
+          size="large"
+          startIcon={<FitnessCenterIcon />}
+        >
           skills
         </Button>
       </Grid>
       <Grid container item xs={6} sm={3} justify="center">
-        <Button size="large" startIcon={<AssignmentTurnedInIcon />}>
+        <Button
+          component={Link}
+          to="works"
+          smooth={true}
+          offset={-100}
+          duration={600}
+          activeClass="active"
+          size="large"
+          startIcon={<AssignmentTurnedInIcon />}
+        >
           works
         </Button>
       </Grid>
       <Grid container item xs={6} sm={3} justify="center">
-        <Button size="large" startIcon={<EmailIcon />}>
+        <Button
+          component={Link}
+          to="contact"
+          smooth={true}
+          offset={-100}
+          duration={600}
+          activeClass="active"
+          size="large"
+          startIcon={<EmailIcon />}
+        >
           contact
         </Button>
       </Grid>
