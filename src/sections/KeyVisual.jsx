ebfcebec73ad721d31c36b2beda46ed8bce62b00
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-09-13 16:42:13 +0900

    白黒を変更

--- src/sections/KeyVisual.jsx
+++ src/sections/KeyVisual.jsx
@@ -1,12 +1,27 @@
 import React from "react";
-import { Grid, Typography, Box } from "@material-ui/core";
+import { Grid, Typography, Box, makeStyles } from "@material-ui/core";
+import grey from "@material-ui/core/colors/grey";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: grey[900],
+  },
+  title: {
+    color: "white",
+  },
+}));
 
 export const KeyVisual = () => {
+  const classes = useStyles();
   return (
-    <Box m={2} mb={16}>
+    <Box m={2} mb={16} className={classes.root}>
       <Grid container alignItems="center" justify="center">
-        <Typography variant="h2">そーたの</Typography>
-        <Typography variant="h2">Portfolio</Typography>
+        <Typography className={classes.title} variant="h2">
+          そーたの
+        </Typography>
+        <Typography className={classes.title} variant="h2">
+          Portfolio
+        </Typography>
       </Grid>
     </Box>
   );
