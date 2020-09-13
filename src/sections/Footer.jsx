67a840c1cdb02296a726f06b809f9babd93047e0
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-09-13 14:34:27 +0900

    margin追加

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -3,7 +3,7 @@ import { Box, Typography, Link } from "@material-ui/core";
 
 export const Footer = () => {
   return (
-    <Box textAlign="center">
+    <Box textAlign="center" m={3}>
       <Typography variant="h10">
         Copyright©2020. Sota Watanabe.{" "}
         <Link href="https://github.com/Sota-Watanabe/Portfolio/">
