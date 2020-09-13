b55c56f4734a31771e04ce2bc5154399c45c15c9
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-09-13 16:26:37 +0900

    googleFormを追加

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import EmailIcon from "@material-ui/icons/Email";
-import { makeStyles, Grid, Box, Typography } from "@material-ui/core";
+import { makeStyles, Box } from "@material-ui/core";
 import grey from "@material-ui/core/colors/grey";
 
 import { SectionLabel } from "../components/SectionLabel";
 
 const useStyles = makeStyles((theme) => ({
-  section: {
-    backgroundColor: grey[700],
-    padding: "50px 0",
+  iframeWapper: {
+    backgroundColor: grey[300],
   },
 }));
 
@@ -17,16 +16,18 @@ export const Contact = () => {
   return (
     <Box id="contact">
       <SectionLabel Icon={EmailIcon} text="CONTACT" />
-      <Grid
-        container
-        justify="center"
-        alignItems="center"
-        className={classes.section}
-      >
-        <Box m={4} my={8}>
-          <Typography variant="h3">現在制作中</Typography>
-        </Box>
-      </Grid>
+      <Box className={classes.iframeWapper}>
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSeaUGVz2LvOh4IJ8EQNQsPr_S7ULdTzev7ER5EBBBsVUNs4hg/viewform?embedded=true"
+          width="100%"
+          height="1000"
+          backgroundcolor="white"
+          frameBorder="0"
+          title="Contact Us"
+        >
+          読み込んでいます…
+        </iframe>
+      </Box>
     </Box>
   );
 };
