9d93db836af898219883a6fdf6b9c8f19147df17
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-09-13 20:53:26 +0900

    blog, twitterを追加, 余白を調整

--- src/sections/KeyVisual.jsx
+++ src/sections/KeyVisual.jsx
@@ -8,7 +8,8 @@ import {
   makeStyles,
 } from "@material-ui/core";
 import GitHubIcon from "@material-ui/icons/GitHub";
-
+import LibraryBooksIcon from "@material-ui/icons/LibraryBooks";
+import TwitterIcon from "@material-ui/icons/Twitter";
 const useStyles = makeStyles((theme) => ({
   customTooltip: {
     fontSize: "15px",
@@ -18,20 +19,54 @@ const useStyles = makeStyles((theme) => ({
 export const KeyVisual = () => {
   const classes = useStyles();
   return (
-    <Box m={2} mb={16}>
-      <Grid container alignItems="center" justify="center">
-        <Typography variant="h2">そーたの</Typography>
-        <Typography variant="h2">Portfolio</Typography>
+    <Box>
+      <Box my={10}>
+        <Grid container alignItems="center" justify="center">
+          <Typography variant="h2">そーたの</Typography>
+          <Typography variant="h2">Portfolio</Typography>
+        </Grid>
+      </Box>
+      <Grid container justify="center">
+        <Tooltip
+          classes={{ tooltip: classes.customTooltip }}
+          title="GitHub"
+          arrow
+        >
+          <IconButton
+            component="a"
+            href="http://github.com/sota-watanabe"
+            target="_blank"
+          >
+            <GitHubIcon fontSize="large" />
+          </IconButton>
+        </Tooltip>
+        <Tooltip
+          classes={{ tooltip: classes.customTooltip }}
+          title="Blog"
+          arrow
+        >
+          <IconButton
+            component="a"
+            href="http://blog.so-ta.net"
+            target="_blank"
+          >
+            <LibraryBooksIcon fontSize="large" />
+          </IconButton>
+        </Tooltip>
+        <Tooltip
+          classes={{ tooltip: classes.customTooltip }}
+          title="Twitter"
+          arrow
+        >
+          <IconButton
+            component="a"
+            href="http://blog.so-ta.net"
+            target="_blank"
+          >
+            <TwitterIcon fontSize="large" />
+          </IconButton>
+        </Tooltip>
       </Grid>
-      <Tooltip
-        classes={{ tooltip: classes.customTooltip }}
-        title="github"
-        arrow
-      >
-        <IconButton aria-label="delete">
-          <GitHubIcon fontSize="large" />
-        </IconButton>
-      </Tooltip>
     </Box>
   );
 };
