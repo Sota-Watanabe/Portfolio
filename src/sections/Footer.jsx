8870f0db33f250dca972d0d01edfdc196b0a8bd1
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-09-13 15:12:58 +0900

    h10->subtitle1に変更

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -4,7 +4,7 @@ import { Box, Typography, Link } from "@material-ui/core";
 export const Footer = () => {
   return (
     <Box textAlign="center" m={3}>
-      <Typography variant="h10">
+      <Typography variant="subtitle1">
         Copyright©2020. Sota Watanabe.{" "}
         <Link href="https://github.com/Sota-Watanabe/Portfolio/">
           Source Code
