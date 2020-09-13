e5c9e401bff8f2f5f18f8249abe144928dd2fd03
Sota-Watanabe <Sota-Watanabe@users.noreply.github.com>
2020-09-13 08:38:56 +0000

    deploy: 1557123236676efde1771ea15603da73ff33f6cf

---
+++ precache-manifest.1f8c43e167c8c51c3ad014392ab4f844.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "485717c66af3d060987ae7195e60c23d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d637b42e24e406d5eadb",
+    "url": "/static/css/main.131bf407.chunk.css"
+  },
+  {
+    "revision": "1e32c40b2713917047f4",
+    "url": "/static/js/2.045cc972.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.045cc972.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d637b42e24e406d5eadb",
+    "url": "/static/js/main.8f246cc1.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.352bdcd6d5ab7fb3838016516fa95db4.js"
+  "/precache-manifest.1f8c43e167c8c51c3ad014392ab4f844.js"
 );
 
 self.addEventListener('message', (event) => {
