585c175e9cd6f964675eafe6bbe48dc8ef87a749
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-09-14 22:53:57 +0900

    使用技術->キーワード

--- src/components/WorkDescription.jsx
+++ src/components/WorkDescription.jsx
@@ -1,22 +1,9 @@
 import React from "react";
-import {
-  Typography,
-  Box,
-  makeStyles,
-  Chip,
-  IconButton,
-} from "@material-ui/core";
+import { Typography, Box, IconButton } from "@material-ui/core";
 import PropTypes from "prop-types";
 import GitHubIcon from "@material-ui/icons/GitHub";
 
-const useStyles = makeStyles((theme) => ({
-  chip: {
-    margin: theme.spacing(0.5),
-  },
-}));
-
 export const WorkDescription = ({ data }) => {
-  const classes = useStyles();
   return (
     <Box mx={3}>
       <Typography variant="h4">{data.name}</Typography>
@@ -25,14 +12,11 @@ export const WorkDescription = ({ data }) => {
       })}
       <Typography>開発期間</Typography>
       {data.period}
-      <Typography>使用技術</Typography>
+      <Typography>キーワード</Typography>
       {data.labels.map((label) => (
-        <Chip
-          variant="outlined"
-          className={classes.chip}
-          label={label}
-          key={label}
-        />
+        <Typography display="inline" key={label}>
+          {label}{" "}
+        </Typography>
       ))}
       <Box textAlign="center">
         <IconButton component="a" href={data.github} target="_blank">
