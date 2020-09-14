6215e0904fafc10654774a3cc8ca114a422d23db
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-09-14 22:19:04 +0900

    データ追加

--- src/data/works/worksData.js
+++ src/data/works/worksData.js
@@ -30,7 +30,7 @@ export const worksData = [
     name: "自作ポートフォリオ",
     imgPath: "images/portfolio.png",
     description:
-      "ゴールデンウィークに作ったもの. 作った slack bot に画像をアップロードすると サーバに保存され,デジタルフォトフレームのスライドショーに追加される.aaaaaa",
+      "このサイトです。\n自分の作品を一覧にしたいなあと思い、作成しました。今までデザインは仲間に任せていたのですが、今回は自分の力だけで作ろうと思ったので、全て自分で制作しました。デザインって大変ですね...。\nまたCI/CDもちゃんとしようということで初めて GitHub Pages、GitHub Actions を利用しました。\n以前から使ってみたかったMaterial-UI も挑戦してみました。\n自分を表現するものを制作するのはとても楽しかったです。",
     labels: [
       "React",
       "Material-UI",
@@ -38,7 +38,7 @@ export const worksData = [
       "Github Actions",
       "Personal",
     ],
-    period: "2week",
+    period: "2weeks",
     github: "https://github.com/Sota-Watanabe/Portfolio",
   },
   {
