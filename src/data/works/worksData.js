61d40a1b1674e00095ce607ec4359f1d7c32e840
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-09-14 21:14:01 +0900

    periodを追加

--- src/data/works/worksData.js
+++ src/data/works/worksData.js
@@ -1,10 +1,19 @@
 export const worksData = [
   {
+    name: "XenonText",
+    imgPath: "images/xenontext.png",
+    description:
+      "ゴールデンウィークに作ったもの. 作った slack bot に画像をアップロードすると サーバに保存され,デジタルフォトフレームのスライドショーに追加される.aaaaaa",
+    labels: ["React", "electron", "tailwind.css", "Team"],
+    period: "5month",
+    github: "https://github.com/cdlab-sit/XenonText",
+  },
+  {
     name: "ピクコレ",
     imgPath: "images/picture_collector.png",
     description:
       "ゴールデンウィークに作ったもの. 作った slack bot に画像をアップロードすると サーバに保存され,デジタルフォトフレームのスライドショーに追加される.",
-    labels: ["python", "flask", "slackbot", "Personal"],
+    labels: ["python", "flask", "slackbot", "RaspberryPi", "Personal"],
     period: "1week",
     github: "https://github.com/Sota-Watanabe/picture-collector",
   },
@@ -14,13 +23,8 @@ export const worksData = [
     description:
       "ゴールデンウィークに作ったもの. 作った slack bot に画像をアップロードすると サーバに保存され,デジタルフォトフレームのスライドショーに追加される.aaaaaa",
     labels: ["python", "flask", "javascript", "HTML", "CSS", "Team"],
-  },
-  {
-    name: "XenonText",
-    imgPath: "images/xenontext.png",
-    description:
-      "ゴールデンウィークに作ったもの. 作った slack bot に画像をアップロードすると サーバに保存され,デジタルフォトフレームのスライドショーに追加される.aaaaaa",
-    labels: ["React", "electron", "tailwind.css", "Team"],
+    period: "1month",
+    github: "https://github.com/cdlab-sit/BBDC",
   },
   {
     name: "自作ポートフォリオ",
@@ -34,12 +38,16 @@ export const worksData = [
       "Github Actions",
       "Personal",
     ],
+    period: "2week",
+    github: "https://github.com/Sota-Watanabe/Portfolio",
   },
   {
     name: "キーボード",
     imgPath: "images/keyboard.png",
     description:
-      "ゴールデンウィークに作ったもの. 作った slack bot に画像をアップロードすると サーバに保存され,デジタルフォトフレームのスライドショーに追加される.aaaaaa",
+      "ゴールデンウィークに作ったもの. 作った slack bot に画像をアップロードすると サーバに保存され, デジタルフォトフレームのスライドショーに追加される.aaaaaa",
     labels: ["python", "flask", "midi", "Personal"],
+    period: "1week",
+    github: "https://github.com/Sota-Watanabe/keyborad",
   },
 ];
