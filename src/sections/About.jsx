98d7769e8b65e502b90c8819d289372cb7b35b91
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-09-20 18:45:33 +0900

    KeyVisualを全画面に変更

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -12,7 +12,10 @@ import {
   Paper,
   TableCell,
 } from "@material-ui/core";
-
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LibraryBooksIcon from "@material-ui/icons/LibraryBooks";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import { IconLink } from "../components/IconLink";
 import { SectionLabel } from "../components/SectionLabel";
 
 const useStyles = makeStyles((theme) => ({
@@ -42,6 +45,23 @@ export const About = () => {
   return (
     <Box id="about">
       <SectionLabel Icon={EmojiPeopleIcon} text="ABOUT" />
+      <Grid container justify="center">
+        <IconLink
+          Icon={GitHubIcon}
+          title="GitHub"
+          link="https://github.com/sota-watanabe"
+        />
+        <IconLink
+          Icon={LibraryBooksIcon}
+          title="Blog"
+          link="http://blog.so-ta.net"
+        />
+        <IconLink
+          Icon={TwitterIcon}
+          title="Twitter"
+          link="https://twitter.com/Sota___Watanabe"
+        />
+      </Grid>
       <Grid
         container
         justify="center"
