303f40a01234c8095b890cca994eed4a05984ab7
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-09-20 15:30:56 +0900

    画像の配置変更

--- src/components/WorkDescription.jsx
+++ src/components/WorkDescription.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography, Box, makeStyles } from "@material-ui/core";
+import { Typography, Box, makeStyles, Grid } from "@material-ui/core";
 import PropTypes from "prop-types";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LibraryBooksIcon from "@material-ui/icons/LibraryBooks";
@@ -26,58 +26,64 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   heading: {
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(0.5),
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(1),
   },
 }));
 
 export const WorkDescription = ({ data }) => {
   const classes = useStyles();
   return (
-    <>
-      <img width="100%" src={data.imgPath} alt={data.name} />
-      <Typography variant="h3" className={classes.title}>
-        {data.name}
-      </Typography>
-      <Box mx={3}>
-        <Box my={2}>
-          <Typography variant="h4" className={classes.heading}>
-            説明
-          </Typography>
-          {data.description.split("\n").map((text, index) => {
-            return <Typography key={index}>{text}</Typography>;
-          })}
-        </Box>
-        <Box my={2}>
-          <Typography variant="h4" className={classes.heading}>
-            開発期間
-          </Typography>
-          {data.period}
-        </Box>
-        <Box>
-          <Typography variant="h4" className={classes.heading}>
-            キーワード
-          </Typography>
-          {data.labels.map((label) => (
-            <Typography display="inline" key={label}>
-              {label}{" "}
+    <Grid container spaceing={1}>
+      <Grid item md={6}>
+        <Typography variant="h2" className={classes.title}>
+          {data.name}
+        </Typography>
+        <Box mx={3}>
+          <Box my={2}>
+            <Typography variant="h3" className={classes.heading}>
+              Description
             </Typography>
-          ))}
-        </Box>
-        <Box my={2} textAlign="center">
-          {data.github && (
-            <IconLink Icon={GitHubIcon} title="GitHub" link={data.github} />
-          )}
+            {data.description.split("\n").map((text, index) => {
+              return <Typography key={index}>{text}</Typography>;
+            })}
+          </Box>
+          <Box my={2}>
+            <Typography variant="h3" className={classes.heading}>
+              Period
+            </Typography>
+            {data.period}
+          </Box>
+          <Box>
+            <Typography variant="h3" className={classes.heading}>
+              Keyword
+            </Typography>
+            {data.labels.map((label) => (
+              <Typography display="inline" key={label}>
+                {label}{" "}
+              </Typography>
+            ))}
+          </Box>
+          <Box my={2} textAlign="center">
+            {data.github && (
+              <IconLink Icon={GitHubIcon} title="GitHub" link={data.github} />
+            )}
 
-          {data.blog && (
-            <IconLink Icon={LibraryBooksIcon} title="Blog" link={data.blog} />
-          )}
-          {data.website && (
-            <IconLink Icon={WebIcon} title="website" link={data.website} />
-          )}
+            {data.blog && (
+              <IconLink Icon={LibraryBooksIcon} title="Blog" link={data.blog} />
+            )}
+            {data.website && (
+              <IconLink Icon={WebIcon} title="website" link={data.website} />
+            )}
+          </Box>
         </Box>
-      </Box>
-    </>
+      </Grid>
+      <Grid item md={6} alignContent="center">
+        <Box my={10}>
+          <img width="100%" height="auto" src={data.imgPath} alt={data.name} />
+        </Box>
+      </Grid>
+    </Grid>
   );
 };
 
