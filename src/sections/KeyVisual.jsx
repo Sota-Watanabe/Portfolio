7f114a4255b0fee81e1a3587fe18023966b3ac66
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-09-25 15:42:20 +0900

    downIcon追加

--- src/sections/KeyVisual.jsx
+++ src/sections/KeyVisual.jsx
@@ -1,9 +1,32 @@
 import React from "react";
-import { Grid, Typography, makeStyles } from "@material-ui/core";
+import {
+  Grid,
+  Typography,
+  makeStyles,
+  Grow,
+  IconButton,
+} from "@material-ui/core";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import { Link } from "react-scroll";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     height: "100vh",
+  },
+  downIcon: {
+    animationName: "$huyohuyo",
+    animationTimingFunction: "ease-in-out",
+    animationIterationCount: "infinite",
+    animationDirection: "alternate",
+    animationDuration: "1.5s",
+  },
+  "@keyframes huyohuyo": {
+    "0%": {
+      transform: "translate(0,0px)",
+    },
+    "100%": {
+      transform: "translate(0,-15px)",
+    },
   },
 }));
 
@@ -16,9 +39,29 @@ export const KeyVisual = () => {
       alignItems="center"
       justify="center"
       color="primary"
+      direction="column"
       className={classes.root}
     >
-      <Typography variant="h1">Sota Watanabe.</Typography>
+      <Typography
+        style={{ marginTop: "10%", marginBottom: "10%" }}
+        variant="h1"
+      >
+        Sota Watanabe.
+      </Typography>
+      <Grow in {...{ timeout: 5000 }}>
+        <IconButton
+          className={classes.downIcon}
+          component={Link}
+          to="about"
+          smooth={true}
+          offset={-100}
+          duration={600}
+          activeClass="active"
+          size="large"
+        >
+          <KeyboardArrowDownIcon style={{ fontSize: 50 }} />
+        </IconButton>
+      </Grow>
     </Grid>
   );
 };
